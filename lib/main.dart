9ba24db6190087e3ca54834d70eb4977c65f8003
hynra <hynra@yahoo.com>
2019-05-30 02:03:06 +0700

    codelab 02 ~ introduction `setState`

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
 
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -32,11 +33,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite: Icons.favorite_border,
+        color: alreadySaved? Colors.red: null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
