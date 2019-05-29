925df58e8e45ce700d007ea5853ef648cd79a35e
hynra <hynra@yahoo.com>
2019-05-30 02:23:10 +0700

    codelab 02 ~ complete

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Welcome to Flutter",
+      theme: ThemeData(
+        primaryColor: Colors.white
+      ),
       home: RandomWords(),
     );
   }
