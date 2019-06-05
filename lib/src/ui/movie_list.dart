46ee79b73fa046779e629ca30791e9ca49ee1b69
hynra <hynra@yahoo.com>
2019-06-05 19:40:39 +0700

    blocs tutorial: change UI to statefull and fetch data at initial state

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
 
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
+  @override
+  MovieListState createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -30,10 +48,12 @@ class MovieList extends StatelessWidget {
         gridDelegate:
         new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data
-                .results[index].poster_path}',
-            fit: BoxFit.cover,
+          return GridTile(
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${snapshot.data
+                  .results[index].poster_path}',
+              fit: BoxFit.cover,
+            ),
           );
         });
   }
