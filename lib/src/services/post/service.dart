b2a51c84d3ca143a110be9aadf96d03f4e78d1a8
hynra <hynra@yahoo.com>
2019-06-09 08:21:06 +0700

    di tutorial: post service

--- lib/src/services/post/service.dart
+++ lib/src/services/post/service.dart
@@ -1,1 +1,18 @@
 import 'package:dependencies/dependencies.dart';
+
+import 'models.dart';
+import '../service.dart';
+
+abstract class PostService implements Service<Post>{}
+
+class PostRestService extends RestServiceBase<Post> implements PostService{
+
+  PostRestService(Injector injector) : super(injector, "posts");
+
+  @override
+  Post toModel(Map map) {
+    return Post.fromMap(map);
+  }
+
+
+}
