1ea50e7a7ee55719852ec193e6e20a8dea52c4e4
hynra <hynra@yahoo.com>
2019-06-09 08:37:01 +0700

    di tutorial: user service

---
+++ lib/src/services/user/service.dart
@@ -0,0 +1,19 @@
+import 'package:dependencies/dependencies.dart';
+
+import 'models.dart';
+import '../service.dart';
+
+abstract class UserService implements Service<User>{}
+
+class UserRestService extends RestServiceBase<User> implements UserService{
+
+  UserRestService(Injector injector, String endPoint) : super(injector, "users");
+
+  @override
+  User toModel(Map map) {
+    return User.fromMap(map);
+  }
+  
+  
+
+}
